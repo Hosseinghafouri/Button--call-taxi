607b7e990f7397a727e72a551bc539dc58496884
Hosseinghafouri <hossenghafouri@gmail.com>
2022-08-04 21:14:56 +0430

    Edit

--- source/script.js
+++ source/script.js
@@ -2,7 +2,6 @@ const $ = document;
 let btnCallTaxi = $.querySelector(".custom-btn");
 let taxiAnimation = $.querySelector(".road");
 let transferToMobile = $.querySelector(".div-btn a");
-// console.log(transferToMobile);
 function callTaxi(params) {
     console.log("Call Taxi");
     btnCallTaxi.style.display = "none";
